6a99b80ee3c320205b0840a77db2c5e3f6d11777
manuelaurreaf <53128590+manuelaurreaf@users.noreply.github.com>
2019-07-28 17:23:14 -0500

    Add files via upload

---
+++ Condicionales1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cálculo de tarifa");
+            Console.WriteLine("Inserte su salario: ");
+            int sal = int.Parse(Console.ReadLine());
+            Console.WriteLine("Inserte la tarifa a: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Inserte la tarifa b: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Inserte la tarifa c: ");
+            int c = int.Parse(Console.ReadLine());
+            int salmin = 828116;
+
+            if (sal < 2 * salmin)
+            {
+                Console.WriteLine("Usted debe pagar la tarifa A:$" + a);
+            }
+            else if ( 2*salmin <= sal && sal <= 4 * salmin)
+            {
+                Console.WriteLine("Usted debe pagar la tarifa B:$" + b);
+            }
+            else
+            {
+                Console.WriteLine("Usted debe pagar la tarifa c:$" + c);
+            }
+
+
+        }
+    }
+}
---
+++ Condicionales2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cálculo de su cuota moderadora para citas generales.");
+            Console.WriteLine("Inserte su salario: ");
+            int sal = int.Parse(Console.ReadLine());
+            int a = 3200;
+            int b = 12700;
+            int c = 33500;
+            int salmin = 828116;
+
+
+            if (sal < 2 * salmin)
+            {
+                Console.WriteLine("Usted debe pagar la tarifa A: " + a);
+            }
+            else if (2 * salmin <= sal && sal <= 5 * salmin)
+            {
+                Console.WriteLine("Usted debe pagar la tarifa B: " + b);
+            }
+            else
+            {
+                Console.WriteLine("Usted debe pagar la tarifa c: " + c);
+            }
+        }
+    }
+}
