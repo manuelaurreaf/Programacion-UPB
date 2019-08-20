7f6bac993ec73cd66b55d5857ebb3c2baf397511
manuelaurreaf <53128590+manuelaurreaf@users.noreply.github.com>
2019-08-20 09:46:11 -0500

    Add files via upload

---
+++ blackjackJugadores.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido a BlackJack");
+            
+            Random aleatorio = new Random();
+            int jugador = 1;
+
+            
+            Console.WriteLine("incerte el numero de jugadores (max 5, min 2)" );
+            int j = int.Parse(Console.ReadLine());
+            while (j < 2 || j > 5)
+            {
+                Console.WriteLine("Error, incerte el numero de jugadores (max 5, min 2)");
+                j = int.Parse(Console.ReadLine());
+            }
+
+            int carta1 = 0, carta2 = 0, total = 0, carta = 0;
+            string respuesta = "s";
+
+            carta1 = aleatorio.Next(1, 11);
+            carta2 = aleatorio.Next(1, 11);
+            total = carta1 + carta2;
+            Console.WriteLine("sus cartas son = " + carta1 + " y " + carta2);
+            Console.WriteLine("Su acumulado es = " + total);
+
+
+
+            while (respuesta == "s" && total < 21)
+            {
+                Console.WriteLine("Bienvenido a BlackJack jugador:" + jugador);
+                
+                carta = aleatorio.Next(1, 11);
+                Console.WriteLine("Su nueva carta es:  " + carta);
+                total += carta;
+                Console.WriteLine("Total: " + total);
+
+                if (total < 22)
+                {
+                    Console.Write("Deseas continuar (s/n): ");
+                    respuesta = Console.ReadLine();
+
+                    Console.WriteLine("Su nueva carta es:  " + carta);
+                    total += carta;
+                    Console.WriteLine("Total: " + total);
+
+                    if (total == 21) Console.WriteLine("¡Ha ganado!");
+
+                }
+                else
+                {
+                    Console.WriteLine("Eliminado");
+                }
+
+            }
+            jugador += 1;
+
+            Console.WriteLine("Su Total fue: " + total);
+            Console.WriteLine("Gracias por jugar.");
+        }
+    }
+}
