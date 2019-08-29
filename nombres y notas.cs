6cbff028b067bb970e2584a8bd732113552fc70e
manuelaurreaf <53128590+manuelaurreaf@users.noreply.github.com>
2019-08-29 09:20:20 -0500

    Add files via upload

---
+++ nombres y notas.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int min = 1000;
+            int auxiliar = 0;
+            string auxiliar2 = "x";
+
+            string[] nombres = { "s", "l", "m", "k", "m" };
+            int[] notas = { 1, 2, 1, 5, 4 };
+
+            for (int i = 0; i < notas.Length; i++)
+            {
+
+                for (int p = i +1 ; p < notas.Length; p++)
+                {
+                    if (notas[i] < notas[p])
+                    {
+                        auxiliar = notas[i];
+                        notas[i] = notas[p];
+                        notas[p] = auxiliar;
+
+                        auxiliar2 = nombres[i];
+                        nombres[i] = nombres[p];
+                        nombres[p] = auxiliar2;
+                    }
+                }
+                Console.WriteLine(notas[i] + " " + nombres[i]);
+            }
+            
+            //for (int i = 0; i < notas.Length ; i++)
+           // {
+                //Console.WriteLine(notas[i] + nombres[i]);
+            //}
+        }
+    }
+}
