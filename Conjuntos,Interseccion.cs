39e0af5cd3d7d88de03749bd7d05d3504137b890
manuelaurreaf <53128590+manuelaurreaf@users.noreply.github.com>
2019-08-30 18:29:31 -0500

    Add files via upload

---
+++ Conjuntos,Interseccion.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int[] A = { 0, 15, 6, 12, 3, 18, 12, 6, 9 };
+            int[] B = { 4, 0, 18, 6, 16, 6, 10, 14, 2, 12, 8, 2 };
+            int[] U = new int[31];
+            int[] Y = new int[31];
+            for (int i = 0; i < U.Length; i++) U[i] = i;
+            //datos
+            for (int i = 0; i < A.Length; i++)
+            {
+                Console.Write(A[i] + ",");
+            }
+            Console.WriteLine(" ");
+            for (int i = 0; i < B.Length; i++)
+            {
+                Console.Write(B[i] + ",");
+            }
+            Console.WriteLine(" ");
+            for (int i = 0; i < U.Length; i++)
+            {
+                Console.Write(U[i] + ",");
+            }
+            Console.WriteLine(" ");
+            int[] norepetidosA = new int[A.Length];
+            for (int i = 0; i < A.Length; i++)
+            {
+                norepetidosA[i] = -1;
+            }
+            for (int i = 0; i < A.Length; i++)
+            {
+                Console.WriteLine(" ");
+                for (int k = 0; k < norepetidosA.Length; k++)
+                {
+                    Console.Write(norepetidosA[k] + ",");
+                }
+
+                int datoactual = A[i];
+                bool repetido = false;
+                for (int j = 0; j < norepetidosA.Length; j++)
+                {
+                    if (datoactual == norepetidosA[j])
+                    {
+                        repetido = true;
+                        break;
+                    }
+                }
+                if (!repetido)
+                {
+                    norepetidosA[i] = datoactual;
+                }
+            }
+            int contador2 = 0;
+            for (int k = 0; k < norepetidosA.Length; k++)
+            {
+                if (norepetidosA[k] != -1)
+                {
+                    contador2 += 1;
+                }
+            }
+            Console.WriteLine("Total no repetidos de A:  " + contador2);
+            int[] norepetidos = new int[B.Length];
+            for (int i = 0; i < B.Length; i++)
+            {
+                norepetidos[i] = -1;
+            }
+            for (int i = 0; i < B.Length; i++)
+            {
+                Console.WriteLine(" ");
+                for (int k = 0; k < norepetidos.Length; k++)
+                {
+                    Console.Write(norepetidos[k] + ",");
+                }
+
+                int datoactual = B[i];
+                bool repetido = false;
+                for (int j = 0; j < norepetidos.Length; j++)
+                {
+                    if (datoactual == norepetidos[j])
+                    {
+                        repetido = true;
+                        break;
+                    }
+                }
+                if (!repetido)
+                {
+                    norepetidos[i] = datoactual;
+                }
+            }
+            int contador = 0;
+            for (int k = 0; k < norepetidos.Length; k++)
+            {
+                if (norepetidos[k] != -1)
+                {
+                    contador += 1;
+                }
+            }
+            Console.WriteLine("Total no repetidos de B:  " + contador);
+            Console.WriteLine(" ");
+            for (int i = 0; i < A.Length; i++)
+            {
+                for (int j = 0; j < B.Length; j++)
+                {
+                    if(A[i] == B[j])
+                    {
+                        Y[a] = A[i];
+                        a++;
+                    }
+                }
+            }
+            List<int> intersec = new List<int>();
+            for (int i = 0; i < B.Length; i++) intersec.Add(Y[i]);
+            intersec.Sort();
+            for (int i = 0; i < intersec.Count - 1; i++)
+            {
+                if (intersec[i] == intersec[i + 1])
+                {
+                    intersec.RemoveAt(i);
+                    i = 0;
+                }
+            }
+            Console.WriteLine("La intersección de los conjuntos A y B es: ");
+            for (int i = 0; i < intersec.Count; i++)
+            {
+                Console.Write(intersec[i] + ",");
+            } 
+        }
+    }
+}
