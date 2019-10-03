7ff215d20fa4116da1862af830e6badf5a1f89df
manuelaurreaf <53128590+manuelaurreaf@users.noreply.github.com>
2019-10-02 20:12:03 -0500

    Add files via upload

---
+++ MotorTorreta.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotorTorreta : MonoBehaviour
+{
+    [SerializeField] float magAng = 360;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Vector3 direccionAng = new Vector3(0, 1, 0);
+        float sentidoAng = Input.GetAxis("HorizontalTorreta");
+        Vector3 velocidadAng = magAng * direccionAng * sentidoAng;
+        Vector3 distanciaAng = velocidadAng * Time.deltaTime;
+        transform.eulerAngles += distanciaAng;
+
+        
+
+    }
+}
