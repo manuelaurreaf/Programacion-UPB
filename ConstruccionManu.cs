58d3aa8013724dffdfb779e3d03dc50e7dc0eb90
manuelaurreaf <53128590+manuelaurreaf@users.noreply.github.com>
2019-10-09 21:24:01 -0500

    Add files via upload

---
+++ ConstruccionManu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using manuela; 
+public class ConstruccionManu : MonoBehaviour
+{
+    [SerializeField] float salud;
+    [SerializeField] MotorManuela construccion; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void destruir()
+    {
+        salud -= construccion.damage /2;
+    }
+}
---
+++ MotorManuela.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace manuela
+{
+    public class MotorManuela : MonoBehaviour
+    {
+        [SerializeField] float magnitud = 8, t = 0, magDash = 500;
+        [SerializeField] float magAng = 450;
+        [SerializeField] public float damage;
+        [SerializeField] AudioClip idle;
+        [SerializeField] AudioClip driving;
+        [SerializeField] float magVelocidad, magAngular, magSalto, salud;
+     
+        AudioSource mAudio;
+        Rigidbody mBody;
+        // Start is called before the first frame update
+        void Start()
+        {
+            mAudio = GetComponent<AudioSource>();
+            mBody = GetComponent<Rigidbody>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //Desplazamiento del tanque
+            Vector3 dirZ = transform.forward;
+            float sentido = Input.GetAxis("Vertical");
+            Vector3 velocidad = magnitud * dirZ * sentido;
+            Vector3 distancia = velocidad * Time.deltaTime;
+            transform.position = transform.position + distancia;
+            //Rotacion del tanque
+            Vector3 dirAng = new Vector3(0, 1, 0);
+            float sAng = Input.GetAxis("Horizontal");
+            Vector3 vAng = magAng * dirAng * sAng;
+            Vector3 distAng = vAng * Time.deltaTime;
+            transform.eulerAngles += distAng;
+
+            if (sentido != 0 || sAng != 0)
+            {
+                mAudio.clip = driving;
+                if (!mAudio.isPlaying) mAudio.Play();
+            }
+            else
+            {
+                mAudio.clip = idle;
+                if (!mAudio.isPlaying) mAudio.Play();
+            }
+            //Dash del tanque con un contador para que no se pueda hacer todo el tiempo
+            t += Time.deltaTime;
+            if (t >= 7)
+            {
+                float sen = 1;
+                Vector3 dash = transform.forward + transform.up;
+                Vector3 force = magDash * dash * sen;
+
+                if (Input.GetButtonDown("Jump"))
+                {
+                    mBody.AddForce(force);
+                    t = 0;
+                }
+            }
+            
+        }
+        //saltar
+        void saltar()
+        {
+            float magSalto = 500;
+            float sen = 1;
+            Vector3 direccion = new Vector3(0, 1, 0);
+            Vector3 salto = magSalto * direccion * sen;
+            mBody.AddForce(salto);
+        }
+        private void OnCollisionStay(Collision collision)
+        {
+            if (collision.gameObject.tag == "Floor" && Input.GetButtonDown("Jump"))
+            {
+                saltar();
+            }
+        }
+        //destruir
+        void destruir()
+        {
+            salud -= 10;
+        }
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.tag == "Proyectil") destruir();
+        }
+    }
+}
